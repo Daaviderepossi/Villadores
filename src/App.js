2b4810926eea94f302dae5d701e13736048bb1d3
Daaviderepossi <daviderepossi2299@gmail.com>
2025-02-15 14:54:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -555,7 +555,8 @@ const App = () => {
   const handleVote = (vote) => {
     if (selectedDate) {
       const dateKey = selectedDate.toDateString();
-      const username = auth.user?.profile?.preferred_username || "Utente Anonimo"; // Usa l'username
+      ##const username = auth.user?.profile?.preferred_username || "Utente Anonimo"; // Usa l'username
+      const username = auth.user?.username || auth.user?.email || "Utente Anonimo";
       setVotes((prevVotes) => ({
         ...prevVotes,
         [dateKey]: {

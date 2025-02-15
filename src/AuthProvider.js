72627d0c6966e2c39723aefb534c682d290a1166
Daaviderepossi <daviderepossi2299@gmail.com>
2025-02-15 12:44:56 +0100

    Update AuthProvider.js

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -4,7 +4,7 @@ import { AuthProvider, AuthService } from "react-oidc-context";
 const oidcConfig = {
   authority: "https://eu-north-1imkpoe97i.auth.eu-north-1.amazoncognito.com",
   client_id: "3jk0r0mrt0d52d2f0oodrk5ks2",
-  redirect_uri: "http://localhost:3000/callback", // DEVE ESSERE UGUALE A QUELLO IN COGNITO
+  redirect_uri: "https://main.d2edpj14zktqpx.amplifyapp.com/callback", // DEVE ESSERE UGUALE A QUELLO IN COGNITO
   response_type: "code",
   scope: "openid profile email",
 };

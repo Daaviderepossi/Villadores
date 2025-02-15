1ceef64908dc24bd251a558039bf8442b2ab3411
Daaviderepossi <daviderepossi2299@gmail.com>
2025-02-15 13:02:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -542,9 +542,12 @@ const App = () => {
   // Reindirizza automaticamente al login se l'utente non è autenticato
   useEffect(() => {
     if (!auth.isLoading && !auth.isAuthenticated) {
-      auth.signOut();
       auth.signinRedirect(); // Reindirizza alla pagina di login di Cognito
     }
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        navigate("/login"); // Reindirizza alla pagina di login se non autenticato
+      }
   }, [auth.isLoading, auth.isAuthenticated, auth]);
 
   // Funzione per gestire il click su una data
